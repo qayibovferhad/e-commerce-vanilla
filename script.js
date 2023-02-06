7e6cc8fbfe6931f2664c806cb8e916d2031f2317
Ferhad <qayibovferhad04@gmail.com>
2023-02-06 17:36:13 +0400

    new commit

--- script.js
+++ script.js
@@ -2,13 +2,99 @@ const productsContainer = document.querySelector("#productsContainer");
 const loadMoreButton = document.querySelector("#loadMoreButton");
 const openCard = document.querySelector("#openCard");
 const cardContainer = document.querySelector("#cardContainer");
+const count = document.querySelector("#count");
+const removeCart = document.querySelector("#removeCart");
 const limit = 12;
 let skip = 0;
 let total = 0;
 fetchProducts();
+let cart = [];
+let savedList = localStorage.getItem("cart_list");
+if (savedList) {
+  savedList = JSON.parse(savedList);
+  cart = savedList;
+  displayCart(cart);
+}
+
+function displayCart(list) {
+  count.innerHTML = cart.length;
+
+  if (cart.length === 0) {
+    const div = document.createElement("div");
+    const text = document.createElement("h1");
+    div.appendChild(text);
+    cardContainer.appendChild(div);
+    text.textContent = "Your Cart Is Empty";
+  }
+
+  let html = "";
+  for (let item of list) {
+    const { title, price, thumbnail } = item;
+    const box = document.getElementById("cardContainer-box");
+    html += `
+    <div class="flex justify-between mt-6">
+         <div class="flex">
+           <img
+           class="h-20 w-20 object-cover rounded"
+             src="${thumbnail}"
+             alt=""
+           />
+           <div class="mx-3">
+             <h3 class="text-sm text-gray-600">${title}</h3>
+             <div class="flex items-center mt-2">
+               <button
+                 class="text-gray-500 focus:outline-none focus:text-gray-600"
+              >
+                 <svg
+                   class="h-5 w-5"
+                 fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  ></path>
+                </svg>
+              </button>
+              <span class="text-gray-700 mx-2">2</span>
+              <button
+                class="text-gray-500 focus:outline-none focus:text-gray-600"
+              >
+                <svg
+                  class="h-5 w-5"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                </svg>
+              </button>
+             </div>
+           </div>
+         </div>
+        <span class="text-gray-600">$${price}</span>
+       </div>
+    `;
+    box.innerHTML = html;
+  }
+}
 productsContainer.addEventListener("click", function (e) {
   if (e.target.tagName === "BUTTON") {
     const { title, thumbnail, price } = e.target.dataset;
+    const newItem = {
+      title: title,
+      thumbnail: thumbnail,
+      price: price,
+    };
+    cart.push(newItem);
+    displayCart(cart);
+    saveList();
   }
 });
 loadMoreButton.addEventListener("click", () => {
@@ -17,6 +103,9 @@ loadMoreButton.addEventListener("click", () => {
 });
 openCard.addEventListener("click", function () {
   cardContainer.classList.replace("translate-x-full", "translate-x-0");
+});
+removeCart.addEventListener("click", function () {
+  cardContainer.classList.replace("translate-x-0", "translate-x-full");
 });
 async function fetchProducts() {
   disableLoadBtn();
@@ -83,4 +172,8 @@ style="background-image: url('${thumbnail}');"
 </div>
 </div>
 `;
+}
+
+function saveList() {
+  localStorage.setItem("cart_item", JSON.stringify(cart));
 }
